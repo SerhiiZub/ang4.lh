85ce800015ec0199198ab494cad26623c859a8df
zub.s <zub.s@novaposhta.ua>
2019-10-18 16:51:34 +0300

    slider

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo.component.css']
 })
 export class LogoComponent implements OnInit {
-  logoUrl: string = '../../assets/img/logo.jpg';
+  logoUrl: string = '../../assets/img/logo.png';
   constructor() { }
 
   ngOnInit() {
